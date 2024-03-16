cf28a6007cbe941ec0bd9f4d097980a527aa9897
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2024-03-16 19:01:37 +0800

    create WebView for In-App browser

---
+++ app/src/main/java/dev/koukeneko/opass/screens/WebViewScreen.kt
@@ -0,0 +1,28 @@
+package dev.koukeneko.opass.screens
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WebViewScreen(
+    url: String = "https://google.com.tw"
+){
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+                settings.setSupportZoom(true)
+            }
+        },
+        update = { webView ->
+            webView.loadUrl(url)
+        }
+    )
+}
