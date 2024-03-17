b85381f687e15e47b663b992d4c83c217b71db5e
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2024-03-17 21:59:31 +0800

    Handle back button press when bottom sheet is expanded

--- app/src/main/java/dev/koukeneko/opass/screens/HomeScreen.kt
+++ app/src/main/java/dev/koukeneko/opass/screens/HomeScreen.kt
@@ -1,4 +1,7 @@
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -6,11 +9,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -24,6 +33,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
@@ -33,9 +43,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -52,6 +65,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.koukeneko.opass.R
@@ -133,11 +147,50 @@ fun HomeScreen(
     )
 
 
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
+    var search_event by remember { mutableStateOf("") }
+    val events = listOf(
+        "SITCON 2024",
+        "DevFest Taipei 2023",
+        "Event 3",
+        "Event 4",
+        "Event 5",
+        "Event 6",
+        "Event 7",
+        "Event 8",
+        "Event 9",
+        "Event 10",
+        "Event 11",
+        "Event 12",
+        "Event 13",
+        "Event 14",
+        "Event 15",
+        "Event 16",
+        "Event 17",
+        "Event 18",
+        "Event 19",
+        "Event 20"
+    )
+    val filteredItems = events.filter { it -> it.contains(search_event, ignoreCase = true) }
 
-    Scaffold(
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            initialValue = SheetValue.Hidden,
+            skipHiddenState = false
+        )
+    )
+
+    // Handle back button press when bottom sheet is expanded
+    // If this is not handled, the back button will navigate to the previous screen ( exit the app )
+    BackHandler(scaffoldState.bottomSheetState.isVisible, onBack = {
+        //hide bottom sheet
+        scope.launch {
+            scaffoldState.bottomSheetState.hide()
+        }
+    })
+
+
+    BottomSheetScaffold(
         topBar = {
             AppBar(
                 subtitle = "KoukeNeko",
@@ -147,9 +200,11 @@ fun HomeScreen(
                         Icon(Icons.Filled.Settings, contentDescription = "Localized description")
                     }
                 },
-        leftIcon = {
+                leftIcon = {
                     IconButton(onClick = {
-                        showBottomSheet = true
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
                     }) {
                         Icon(
                             painter = painterResource(id = R.drawable.rounded_stack_24),
@@ -159,12 +214,135 @@ fun HomeScreen(
                     }
                 }
             )
-        }
-    ) {
+        },
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 10.dp, end = 10.dp),
+            ) {
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            TextField(
+                                singleLine = true,
+                                leadingIcon = {
+                                    Icon(
+                                        Icons.Rounded.Search,
+                                        contentDescription = "Localized description"
+                                    )
+                                },
+                                label = { Text("搜尋活動") },
+                                modifier = Modifier.fillParentMaxWidth(),
+                                value = search_event,
+                                onValueChange = {
+                                    search_event = it
+                                })
+                        }
+                    }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+                itemsIndexed(filteredItems) { index, event ->
+                    val shape = when (index) {
+                        0 -> if (filteredItems.size == 1) {
+                            // If there's only one item, apply rounded corners to all sides
+                            RoundedCornerShape(10.dp)
+                        } else {
+                            // First item, only round the top corners
+                            RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
+                        }
+
+                        filteredItems.lastIndex -> RoundedCornerShape(
+                            bottomStart = 10.dp,
+                            bottomEnd = 10.dp
+                        )
+
+                        else -> RoundedCornerShape(0.dp)
+                    }
+                    Card(
+                        shape = shape,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 10.dp, end = 10.dp)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { /*TODO*/ }
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Image(
+                                    imageVector = SITCON_white(),
+                                    contentDescription = event,
+                                    modifier = Modifier
+                                        .padding(10.dp)
+                                        .height(70.dp)
+                                        .width(100.dp)
+                                )
+                                Text(
+                                    text = event,
+                                )
+                            }
+                            Icon(
+                                imageVector = chevron_right(),
+                                contentDescription = event,
+                                modifier = Modifier
+                                    .padding(end = 10.dp)
+                                    .height(30.dp),
+                                tint = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                    }
+                    if (index < events.lastIndex) {
+                        Divider(
+                            color = Color.Transparent,
+                            thickness = 1.dp,
+                        )
+                    }
+                }
+                if (filteredItems.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "找不到符合的活動")
+                        }
+                    }
+                } else {
+                    item {
+                        Spacer(modifier = Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
+                    }
+                }
+
+                item {
+                    Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.ime)) //can remove
+                }
+            }
+
+        }) { innerPadding ->
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .padding(it)
+                .padding(innerPadding)
                 .fillMaxSize()
         ) {
 
@@ -173,7 +351,6 @@ fun HomeScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(200.dp)
-//                    .background(Color.Red)
             ) {
                 Image(
                     SITCON_white(),
@@ -182,7 +359,6 @@ fun HomeScreen(
                         .width(250.dp)
                         .height(200.dp)
                         .align(Alignment.BottomCenter)
-//                        .background(Color.Magenta)
                 )
             }
             Spacer(modifier = Modifier.height(10.dp))
@@ -199,118 +375,6 @@ fun HomeScreen(
                         onClick = button.onClick,
                         color = MaterialTheme.colorScheme.primary
                     )
-                }
-            }
-        }
-        var search_event by remember { mutableStateOf("") }
-        val events = listOf("SITCON 2024", "DevFest Taipei 2023", "Event 3", "Event 4", "Event 5", "Event 6", "Event 7", "Event 8", "Event 9", "Event 10","Event 11", "Event 12", "Event 13", "Event 14", "Event 15", "Event 16", "Event 17", "Event 18", "Event 19", "Event 20")
-        val filteredItems = events.filter { it -> it.contains(search_event, ignoreCase = true) }
-
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
-            ) {
-
-                Column(
-                    modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp),
-                ) {
-
-                    LazyColumn {
-                        item {
-                            Card(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(10.dp)
-                            ) {
-                                Row(
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                ) {
-                                    TextField(
-
-                                        label = { Row {
-                                            Icon(Icons.Rounded.Search, contentDescription = "Localized description")
-                                            Text("搜尋活動")
-                                        } },
-                                        modifier = Modifier.fillParentMaxWidth(),
-                                        value = search_event,
-                                        onValueChange = {
-                                            search_event = it
-                                    })
-                                    Button(
-                                        modifier = Modifier.width(50.dp),
-                                        onClick = { /*TODO*/ }) {
-//                                search icon
-                                        Icon(Icons.Filled.Search, contentDescription = "Localized description")
-                                    }
-                                }
-                            }
-                        }
-                        item {
-                            Spacer(modifier = Modifier.height(10.dp))
-                        }
-                        itemsIndexed(filteredItems) { index, event ->
-                            val shape = when (index) {
-                                0 -> RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
-                                events.lastIndex -> RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp)
-                                else -> RoundedCornerShape(0.dp)
-                            }
-                            Card(
-                                shape = shape,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(start = 10.dp, end = 10.dp)
-                            ) {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .clickable { /*TODO*/ }
-                                ) {
-                                    Row(
-                                        verticalAlignment = Alignment.CenterVertically,
-                                    ) {
-                                        Image(imageVector = SITCON_white(), contentDescription = event, modifier = Modifier
-                                            .padding(10.dp)
-                                            .height(70.dp)
-                                            .width(100.dp))
-                                        Text(
-                                            text = event,
-                                        )
-                                    }
-                                    Icon(imageVector = chevron_right(), contentDescription = event, modifier = Modifier
-                                        .padding(end = 10.dp)
-                                        .height(30.dp), tint = MaterialTheme.colorScheme.onSurface)
-                                }
-                            }
-                            if (index < events.lastIndex) {
-                                Divider(
-                                    color = Color.Transparent,
-                                    thickness = 1.dp,
-                                )
-                            }
-                        }
-                        item {
-                            Spacer(modifier = Modifier.height(20.dp))
-                        }
-                    }
-                }
-                // Sheet content
-                Button(onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        if (!sheetState.isVisible) {
-                            showBottomSheet = false
-                        }
-                    }
-                }) {
-                    Text("Hide bottom sheet")
                 }
             }
         }
